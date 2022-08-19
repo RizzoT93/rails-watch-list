a6ead027fea49b63893197e896ef76c852927843
Tais Rizzo <rizzotais@gmail.com>
2022-08-19 11:50:36 -0400

    k

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,37 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
-    @bookmarks = Bookmark.new
+    @bookmark = Bookmark.new
   end
 
   def create
-    @bookmarks = Bookmark.new(bookmark_params)
-    @bookmarks.save
-    redirect_to bookmark_path
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @lists
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
-    @bookmarks = Bookmark.find(params[:id])
-    @bookmarks = Bookmark.destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,22 +4,22 @@ class ListsController < ApplicationController
   end
 
   def new
-    @lists = List.new
+    @list = List.new
   end
 
   def create
-    @lists = List.new(lists_params)
-    @lists.save
+    @list = List.new(list_params)
+    @list.save
     redirect_to lists_path
   end
 
   def show
-    @lists = List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   private
 
-  def lists_params
-    params.require(:lists).permit(:name)
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
